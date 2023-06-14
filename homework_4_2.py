d3bc7834602e0607e8523e362b461b51c044de71
Leodgar2010 <leodgar2010@yandex.ru>
2023-06-14 21:37:51 +0300

    "Forth homework"

---
+++ homework_4_2.py
@@ -0,0 +1,15 @@
+# Напишите функцию принимающую на вход только ключевые
+# параметры и возвращающую словарь, где ключ — значение
+# переданного аргумента, а значение — имя аргумента. Если
+# ключ не хешируем, используйте его строковое представление.
+def my_func(**kwargs):
+    dic = {}
+    for i, j in kwargs.items():
+        if isinstance(j, list) or isinstance(j, dict) or isinstance(j, set):
+            dic[str(j)] = i
+        else:
+            dic[j] = i
+    print(dic)
+
+
+my_func(a=[1, 2, 3], b={2}, c={3: 'value'}, d="output")
